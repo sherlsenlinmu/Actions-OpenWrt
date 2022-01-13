cc62b2358ed1fffc838c550e12ca665c26a4ef5d
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2022-01-13 18:27:51 +0000

    Create diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -71,6 +71,7 @@ sed -i 's/"CPU 性能优化调节"/"CPU 调频"/g'  package/lean/luci-app-cpufre
 ./scripts/feeds install -a
 
 #revert routing
-#cd feeds/routing
-#git reset –hard e43c272acb03255e47c2321e9e3ac935d437f3c3
-#popd
+rm -rf feeds/routing
+git clone https://github.com/openwrt/routing feeds/routing
+cd feeds/routing && git reset –hard e43c272acb03255e47c2321e9e3ac935d437f3c3
+popd
