6ea3cd40fc269b3992e3c9130f45038fa0887f29
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2022-01-13 19:03:27 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '3d' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
