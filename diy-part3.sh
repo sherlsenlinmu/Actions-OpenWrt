d2f7375d64404bcc4e19d29a9a585836ba4c79cd
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2022-01-18 22:47:27 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -23,23 +23,37 @@ rm -rf package/lean/luci-app-unblockmusic
 rm -rf package/lean/UnblockNeteaseMusic-Go
 rm -rf package/lean/luci-app-jd-dailybonus
 rm -rf package/lean/luci-app-pushbot
+rm -rf package/lean/trojan
+rm -rf package/lean/simple-obfs
+rm -rf package/lean/vsftpd-alt
+rm -rf package/lean/tcpping
+rm -rf package/lean/microsocks
+rm -rf package/lean/dns2socks
+rm -rf package/lean/ipt2socks
+rm -rf package/lean/luci-app-webadmin
+rm -rf package/lean/luci-app-ttyd
+rm -rf package/lean/luci-app-syncdial
+rm -rf package/lean/luci-app-pptp-server
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-mwan3helper
+rm -rf package/lean/luci-app-filetransfer
+rm -rf package/lean/ipv6-helper
+rm -rf package/lean/ddns-scripts_dnspod
 
 # Modify default
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
 sed -i 's#root::0:0:99999:7:::#root:$1$wEehtjxj$YBu4quNfVUjzfv8p/PBo5.:0:0:99999:7:::#g' package/base-files/files/etc/shadow
-sed -i 's/R22.1.1/R22.1.15/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R21.7.1.100/R22.1.19/g' package/lean/default-settings/files/zzz-default-settings
 #sed -i 's#root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::#root:$1$wEehtjxj$YBu4quNfVUjzfv8p/PBo5.:0:0:99999:7:::#g' package/lean/default-settings/files/zzz-default-settings
-sed -i '34d' package/lean/default-settings/files/zzz-default-settings
+sed -i '33d' package/lean/default-settings/files/zzz-default-settings
 
 # 添加额外软件包
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/apps/luci-app-openclash
 git clone https://github.com/rufengsuixing/luci-app-zerotier package/lean/luci-app-zerotier
 git clone https://github.com/small-5/luci-app-adblock-plus package/apps/luci-app-adblock-plus
-svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/apps/luci-app-filebrowser
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-adguardhome package/apps/luci-app-adguardhome
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-smartdns package/apps/luci-app-smartdns
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-advanced package/apps/luci-app-advanced
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-timewol package/apps/luci-app-control-timewol
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-eqos package/apps/luci-app-eqos
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-iptvhelper package/apps/luci-app-iptvhelper
@@ -51,15 +65,28 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-socat package
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-timecontrol package/apps/luci-app-timecontrol
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/lean/luci-app-netdata
 git clone https://github.com/ntlf9t/luci-app-easymesh package/lean/luci-app-easymesh
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ package/lean/
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ package/lean/
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ package/lean/
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ package/lean/
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ package/lean/
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ package/lean/
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ package/lean/
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ package/lean/
-
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd package/lean/luci-app-vlmcsd
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd package/lean/vlmcsd
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-aliyundrive-webdav package/lean/luci-app-aliyundrive-webdav
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/aliyundrive-webdav package/lean/aliyundrive-webdav
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-unblockmusic package/lean/luci-app-unblockmusic
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/UnblockNeteaseMusic-Go package/lean/UnblockNeteaseMusic-Go
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-jd-dailybonus package/lean/luci-app-jd-dailybonus
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-pushbot package/lean/luci-app-pushbot
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vsftpd-alt package/lean/vsftpd-alt
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping package/lean/tcpping
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks package/lean/microsocks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks package/lean/dns2socks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks package/lean/ipt2socks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-webadmin package/lean/luci-app-webadmin
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ttyd package/lean/luci-app-ttyd
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-syncdial package/lean/luci-app-syncdial
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-pptp-server package/lean/luci-app-pptp-server
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-pppoe-relay package/lean/luci-app-pppoe-relay
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-mwan3helper package/lean/luci-app-mwan3helper
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-filetransfer package/lean/luci-app-filetransfer
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper package/lean/ipv6-helper
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_dnspod package/lean/ddns-scripts_dnspod
 
 # luci-app-easymesh
 sed -i 's/wpad-mesh-openssl/wpad-openssl/g' package/lean/luci-app-easymesh/Makefile
