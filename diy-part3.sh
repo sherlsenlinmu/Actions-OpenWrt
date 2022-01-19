c2a946774003d13230afdd8e3313cb811a072cd6
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2022-01-20 01:53:30 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -53,7 +53,7 @@ rm -rf feeds/packages/admin/netdata
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
 sed -i 's#root::0:0:99999:7:::#root:$1$wEehtjxj$YBu4quNfVUjzfv8p/PBo5.:0:0:99999:7:::#g' package/base-files/files/etc/shadow
-sed -i 's/R21.7.1.100/R22.1.19/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R21.7.1.100/R22.1.20/g' package/lean/default-settings/files/zzz-default-settings
 #sed -i 's#root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::#root:$1$wEehtjxj$YBu4quNfVUjzfv8p/PBo5.:0:0:99999:7:::#g' package/lean/default-settings/files/zzz-default-settings
 sed -i '33d' package/lean/default-settings/files/zzz-default-settings
 
