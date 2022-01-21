e87711b2f084fc5cc76c74d34ac98611304ece46
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2022-01-21 19:58:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -47,7 +47,7 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-rebootschedul
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-socat package/apps/luci-app-socat
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-timecontrol package/apps/luci-app-timecontrol
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/lean/luci-app-netdata
-#git clone https://github.com/sherlsenlinmu/luci-app-easymesh package/lean/luci-app-easymesh
+git clone https://github.com/sherlsenlinmu/luci-app-easymesh package/lean/luci-app-easymesh
 
 # luci-app-easymesh
 #sed -i 's/wpad-mesh-openssl/wpad-openssl/g' package/lean/luci-app-easymesh/Makefile
