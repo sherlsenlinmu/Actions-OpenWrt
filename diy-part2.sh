2a89883c537a4db6cc7bfc79d7df39ff17e31cd7
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2022-02-16 14:10:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,10 @@
 #
 
 # 删除软件包
-rm -rf package/lean/luci-app-zerotier
+rm -rf feeds/luci/applications/luci-app-zerotier
 rm -rf package/lean/mentohust
-rm -rf package/lean/luci-app-netdata
-rm -rf package/lean/luci-app-easymesh
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-easymesh
 
 # autocore
 sed -i 's/DEPENDS:=@(.*/DEPENDS:=@(TARGET_bcm27xx||TARGET_bcm53xx||TARGET_ipq40xx||TARGET_ipq806x||TARGET_ipq807x||TARGET_mvebu||TARGET_rockchip||TARGET_armvirt) \\/g' package/lean/autocore/Makefile
@@ -31,7 +31,7 @@ sed -i '34d' package/lean/default-settings/files/zzz-default-settings
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/apps/luci-app-openclash
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/apps/luci-app-amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall package/apps/passwall
-git clone https://github.com/rufengsuixing/luci-app-zerotier package/lean/luci-app-zerotier
+git clone https://github.com/rufengsuixing/luci-app-zerotier package/apps/luci-app-zerotier
 git clone https://github.com/small-5/luci-app-adblock-plus package/apps/luci-app-adblock-plus
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/apps/luci-app-filebrowser
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-adguardhome package/apps/luci-app-adguardhome
@@ -46,15 +46,15 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/mentohust package/lean
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-rebootschedule package/apps/luci-app-rebootschedule
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-socat package/apps/luci-app-socat
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-timecontrol package/apps/luci-app-timecontrol
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/lean/luci-app-netdata
-git clone https://github.com/ntlf9t/luci-app-easymesh package/lean/luci-app-easymesh
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/apps/luci-app-netdata
+git clone https://github.com/ntlf9t/luci-app-easymesh package/apps/luci-app-easymesh
 
 # luci-app-easymesh
-sed -i 's/wpad-mesh-openssl/wpad-openssl/g' package/lean/luci-app-easymesh/Makefile
+sed -i 's/wpad-mesh-openssl/wpad-openssl/g' package/apps/luci-app-easymesh/Makefile
 
 # 删除docker无脑初始化教程
-sed -i '31,39d' package/lean/luci-app-docker/po/zh-cn/docker.po
-rm -rf package/lean/luci-app-docker/root/www
+sed -i '31,39d' feeds/luci/applications/luci-app-docker/po/zh-cn/docker.po
+rm -rf feeds/luci/applications/luci-app-docker/root/www
 
 # 晶晨宝盒
 sed -i "s|https.*/amlogic-s9xxx-openwrt|https://github.com/breakings/OpenWrt|g" package/apps/luci-app-amlogic/root/etc/config/amlogic
@@ -63,9 +63,9 @@ sed -i "s|s9xxx_lede|ARMv8|g" package/apps/luci-app-amlogic/root/etc/config/amlo
 #sed -i "s|.img.gz|..OPENWRT_SUFFIX|g" package/apps/luci-app-amlogic/root/etc/config/amlogic
 
 # readd cpufreq for aarch64
-sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
-sed -i 's/services/system/g'  package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
-sed -i 's/"CPU 性能优化调节"/"CPU 调频"/g'  package/lean/luci-app-cpufreq/po/zh-cn/cpufreq.po
+sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' feeds/luci/applications/luci-app-cpufreq/Makefile
+sed -i 's/services/system/g'  feeds/luci/applications/luci-app-cpufreq/luasrc/controller/cpufreq.lua
+sed -i 's/"CPU 性能优化调节"/"CPU 调频"/g'  feeds/luci/applications/luci-app-cpufreq/po/zh-cn/cpufreq.po
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
