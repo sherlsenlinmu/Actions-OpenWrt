120e0061e21227259315f67b20a92635dc94bde6
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2022-04-25 01:05:38 +0800

    Update ax6-1.sh

--- ax6-1.sh
+++ ax6-1.sh
@@ -12,11 +12,15 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '4d' feeds.conf.default
 sed -i '3d' feeds.conf.default
+sed -i '2d' feeds.conf.default
 sed -i '1d' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git routing https://git.openwrt.org/feed/routing.git^4f235865e7d8c37b4ea7d8ee55b4e603a11e06b7' >>feeds.conf.default
 echo 'src-git packages https://github.com/coolsnowwolf/packages.git^fd95b1a5f9c1130339c4e61bc6061ae55e04220d' >>feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci^5f363d93c5f6ec011ac3d63b6e15be23e27ecdb7' >>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git^561131d523d387df4c61e3e9851ea777b5a03408' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
