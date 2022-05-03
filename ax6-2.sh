49478cff45ac46e8c53fcabc247cee2360f0132f
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2022-05-03 12:58:39 +0800

    Update ax6-2.sh

--- ax6-2.sh
+++ ax6-2.sh
@@ -12,7 +12,7 @@
 
 # 删除软件包
 rm -rf package/lean/luci-app-zerotier
-rm -rf package/lean/luci-app-easymesh
+#rm -rf package/lean/luci-app-easymesh
 rm -rf package/lean/luci-app-vlmcsd
 rm -rf package/lean/vlmcsd
 rm -rf package/lean/luci-app-aliyundrive-webdav
@@ -23,7 +23,6 @@ rm -rf package/lean/luci-app-pushbot
 rm -rf package/lean/autocore
 rm -rf feeds/packages/net/zerotier
 rm -rf feeds/packages/net/smartdns
-rm -rf feeds/packages/net/dawn
 
 # Modify default
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
@@ -36,11 +35,7 @@ sed -i '33d' package/lean/default-settings/files/zzz-default-settings
 # 添加额外软件包
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/apps/luci-app-openclash
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-dnsfilter package/apps/luci-app-dnsfilter
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/apps/luci-app-adguardhome
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-smartdns package/apps/luci-app-smartdns
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-iptvhelper package/apps/luci-app-iptvhelper
-svn co https://github.com/kiddin9/openwrt-packages/trunk/iptvhelper package/apps/iptvhelper
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-socat package/apps/luci-app-socat
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/lean/luci-app-vlmcsd
 svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/lean/vlmcsd
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-aliyundrive-webdav package/lean/luci-app-aliyundrive-webdav
@@ -48,18 +43,16 @@ svn co https://github.com/kiddin9/openwrt-packages/trunk/aliyundrive-webdav pack
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/lean/luci-app-unblockmusic
 svn co https://github.com/kiddin9/openwrt-packages/trunk/UnblockNeteaseMusic-Go package/lean/UnblockNeteaseMusic-Go
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-pushbot package/lean/luci-app-pushbot
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-koolproxyR package/apps/luci-app-koolproxyR
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/autocore package/lean/autocore
 svn co https://github.com/coolsnowwolf/packages/trunk/net/zerotier feeds/packages/net/zerotier
 svn co https://github.com/coolsnowwolf/packages/trunk/net/smartdns feeds/packages/net/smartdns
-svn co https://github.com/coolsnowwolf/packages/trunk/net/dawn feeds/packages/net/dawn
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall package/apps/passwall
 git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall package/apps/luci-app-passwall
 git clone https://github.com/rufengsuixing/luci-app-zerotier package/lean/luci-app-zerotier
 
 # luci-app-easymesh
-git clone https://github.com/ntlf9t/luci-app-easymesh package/lean/luci-app-easymesh
-sed -i 's/wpad-mesh-openssl/wpad-openssl/g' package/lean/luci-app-easymesh/Makefile
+#git clone https://github.com/ntlf9t/luci-app-easymesh package/lean/luci-app-easymesh
+#sed -i 's/wpad-mesh-openssl/wpad-openssl/g' package/lean/luci-app-easymesh/Makefile
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
