73fe0e5b8a740283c4b7c77a326cc984a7afc15f
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2022-07-16 00:04:43 +0800

    Update ax6-2.sh

--- ax6-2.sh
+++ ax6-2.sh
@@ -28,7 +28,7 @@ rm -rf feeds/packages/net/smartdns
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
 sed -i 's#root::0:0:99999:7:::#root:$1$wEehtjxj$YBu4quNfVUjzfv8p/PBo5.:0:0:99999:7:::#g' package/base-files/files/etc/shadow
-sed -i 's/R21.7.1.100/R22.6.15/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R21.7.1.100/R22.7.15/g' package/lean/default-settings/files/zzz-default-settings
 #sed -i 's#root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::#root:$1$wEehtjxj$YBu4quNfVUjzfv8p/PBo5.:0:0:99999:7:::#g' package/lean/default-settings/files/zzz-default-settings
 sed -i '33d' package/lean/default-settings/files/zzz-default-settings
 
@@ -44,14 +44,23 @@ svn co https://github.com/kiddin9/openwrt-packages/trunk/aliyundrive-webdav pack
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblockmusic package/lean/luci-app-unblockmusic
 svn co https://github.com/kiddin9/openwrt-packages/trunk/UnblockNeteaseMusic-Go package/lean/UnblockNeteaseMusic-Go
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-pushbot package/lean/luci-app-pushbot
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-argonne package/apps/luci-theme-argonne
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-argonne-config package/apps/luci-app-argonne-config
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-pppoe-server package/apps/luci-app-pppoe-server
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-ikoolproxy package/apps/luci-app-ikoolproxy
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-minieap package/apps/luci-app-minieap
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-iptvhelper package/apps/luci-app-iptvhelper
+svn co https://github.com/kiddin9/openwrt-packages/trunk/iptvhelper package/apps/iptvhelper
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/autocore package/lean/autocore
 svn co https://github.com/coolsnowwolf/packages/trunk/net/zerotier feeds/packages/net/zerotier
 svn co https://github.com/coolsnowwolf/packages/trunk/net/smartdns feeds/packages/net/smartdns
 #svn co https://github.com/coolsnowwolf/packages/trunk/net/cloudflared feeds/packages/net/cloudflared
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall package/apps/passwall
-#git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall package/apps/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2 package/apps/luci-app-passwall2
+git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall package/apps/luci-app-passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall2 package/apps/luci-app-passwall2
 git clone https://github.com/rufengsuixing/luci-app-zerotier package/lean/luci-app-zerotier
+git clone -b default --single-branch https://github.com/AutoCONFIG/minieap-openwrt.git package/apps/minieap-openwrt
+
 
 # luci-app-easymesh
 #git clone https://github.com/ntlf9t/luci-app-easymesh package/lean/luci-app-easymesh
