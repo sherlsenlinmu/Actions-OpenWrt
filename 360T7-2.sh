2854f9348177d0d203d2a3b2b5a5cf91fb6aa1a8
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2023-01-01 16:16:18 +0800

    Update 360T7-2.sh

--- 360T7-2.sh
+++ 360T7-2.sh
@@ -18,13 +18,8 @@ rm -rf feeds/luci/applications/luci-app-socat
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
 sed -i 's#root::0:0:99999:7:::#root:$1$wEehtjxj$YBu4quNfVUjzfv8p/PBo5.:0:0:99999:7:::#g' package/base-files/files/etc/shadow
-#sed -i 's/R22.11.11/R22.11.15/g' package/lean/default-settings/files/zzz-default-settings
 
 # 添加额外软件包
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/apps/luci-app-openclash
-
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-smartdns package/apps/luci-app-smartdns
-
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-timewol package/apps/luci-app-control-timewol
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-weburl package/apps/luci-app-control-weburl
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-rebootschedule package/apps/luci-app-rebootschedule
@@ -32,6 +27,11 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-socat package
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-dnsfilter package/apps/luci-app-dnsfilter
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-eqos package/apps/luci-app-eqos
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-pppoe-server package/apps/luci-app-pppoe-server
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-pptp-server package/apps/luci-app-pptp-server
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-pushbot package/apps/luci-app-pushbot
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-turboacc package/apps/luci-app-turboacc
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-guest-wifi package/apps/luci-app-guest-wifi
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-ipsec-server package/apps/luci-app-ipsec-server
 git clone https://github.com/rufengsuixing/luci-app-zerotier package/apps/luci-app-zerotier
 
 ./scripts/feeds update -a
