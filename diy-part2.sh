b88e354f17ff542d01c9583a05d93b8a4c77fa6f
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2024-06-11 09:50:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,27 +23,27 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
 sed -i 's#root:::0:99999:7:::#root:$1$wEehtjxj$YBu4quNfVUjzfv8p/PBo5.:0:0:99999:7:::#g' package/base-files/files/etc/shadow
-#sed -i 's/R24.5.1/R24.6.1/g' package/lean/default-settings/files/zzz-default-settings
-#sed -i '34d' package/lean/default-settings/files/zzz-default-settings
-#sed -i '34d' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R24.5.1/R24.6.1/g' package/lean/default-settings/files/zzz-default-settings
+sed -i '34d' package/lean/default-settings/files/zzz-default-settings
+sed -i '34d' package/lean/default-settings/files/zzz-default-settings
 sed -i "s/DISTRIB_ID='%D'/DISTRIB_ID='OpenWrt'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/DISTRIB_RELEASE='%V'/DISTRIB_RELEASE=''/g" package/base-files/files/etc/openwrt_release
-sed -i "s/DISTRIB_REVISION='%R'/DISTRIB_REVISION='R24.6.1'/g" package/base-files/files/etc/openwrt_release
-sed -i "s/DISTRIB_DESCRIPTION='%D %V %C'/DISTRIB_DESCRIPTION='OpenWrt R24.6.1 '/g" package/base-files/files/etc/openwrt_release
+#sed -i "s/DISTRIB_REVISION='%R'/DISTRIB_REVISION='R24.6.1'/g" package/base-files/files/etc/openwrt_release
+#sed -i "s/DISTRIB_DESCRIPTION='%D %V %C'/DISTRIB_DESCRIPTION='OpenWrt R24.6.1 '/g" package/base-files/files/etc/openwrt_release
 sed -i 's/NAME="%D"/NAME="OpenWrt"/g' package/base-files/files/usr/lib/os-release
 sed -i 's/VERSION="%V"/VERSION=""/g' package/base-files/files/usr/lib/os-release
 sed -i 's/PRETTY_NAME="%D %V"/PRETTY_NAME="OpenWrt"/g' package/base-files/files/usr/lib/os-release
 sed -i 's/VERSION_ID="%v"/VERSION_ID=""/g' package/base-files/files/usr/lib/os-release
 sed -i 's/OPENWRT_RELEASE="%D %V %C"/OPENWRT_RELEASE="OpenWrt %C"/g' package/base-files/files/usr/lib/os-release
-sed -i 's/%D %V/Base on OpenWrt by OpenWrt/g' package/base-files/files/etc/banner
+sed -i 's/%D %V/Base on OpenWrt by Lean/g' package/base-files/files/etc/banner
 
 # 添加额外软件包
 #git clone -b master --single-branch https://github.com/vernesong/OpenClash package/luci-app-openclash
 #git clone https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
 git clone https://github.com/sherlsenlinmu/apps package/apps
 git clone https://github.com/kiddin9/luci-app-dnsfilter package/luci-app-dnsfilter
-#git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-#git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
 git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 #git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
@@ -51,7 +51,7 @@ git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-auto
 #git clone https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
 #git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
 #git clone https://github.com/sirpdboy/luci-app-lucky package/luci-app-lucky
-git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
+#git clone https://github.com/kiddin9/openwrt-packages package/openwrt-packages
 
 
 # 晶晨宝盒
