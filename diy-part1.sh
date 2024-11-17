a2955204d6ead0e3e94190f3bd33213e29ae5812
littlesherl <71514869+sherlsenlinmu@users.noreply.github.com>
2024-11-17 15:40:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,10 @@
 #
 
 # Uncomment a feed source
-#sed -i '3d' feeds.conf.default
-#sed -i '1d' feeds.conf.default
+sed -i '3d' feeds.conf.default
+sed -i '2d' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
 #echo 'src-git routing https://git.openwrt.org/feed/routing.git^fffa9cb161acee226f1080cda2a3d53ded656f14' >>feeds.conf.default
 #echo 'src-git packages https://github.com/coolsnowwolf/packages.git^fd95b1a5f9c1130339c4e61bc6061ae55e04220d' >>feeds.conf.default
